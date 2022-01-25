be7f1e8fa20620e5f4a09435c6bd6f14855b4598
Sumanth1919 <96106917+Sumanth1919@users.noreply.github.com>
2022-01-25 15:14:03 +0530

    Add files via upload

---
+++ content.java
@@ -0,0 +1,9 @@
+public content
+{
+    public static void main(String args[])
+    {
+        System.out.println("succes");
+        System.out.println("ok");
+        System.out.println("see");
+    }
+}
